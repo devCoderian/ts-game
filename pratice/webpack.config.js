6b550a0eb909184c2733a40643ec3a21ed96550a
devCoderian <devCoderian@gmail.com>
2021-09-24 16:19:29 +0900

    ts & webpack setting

---
+++ pratice/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpack = require('path');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'eval', //hidden-source-map
+    resolve: {
+        extensions : ['.jsx', 'js', '.tsx', '.ts']
+    },
+    entry: {
+        app: './client'
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'awesome-typescript-loader'
+        }]
+    }, // loader
+    plugins: [
+
+    ], // {array},
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, 'dist'),
+    }
+}
